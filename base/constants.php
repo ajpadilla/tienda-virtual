71f80dab7fc99687054cfcaf563016578125cca6
ajpadilla <ajpadilla88@gmail.com>
2015-11-25 22:49:16 -0430

    Agregando archivo para constantes php del proyecto

---
+++ base/constants.php
@@ -0,0 +1,21 @@
+<?php 
+
+# modules
+const MODULEBASE = 'base/'
+const MODULEUSER = 'users/';
+
+# actions forms 
+const SET_USER = 'set';
+const GET_USER = 'get';
+const DELETE_USER = 'delete';
+const EDIT_USER = 'edit';
+
+# views
+/*const VIEW_SET_USER = 'add';
+const VIEW_GET_USER = 'search';
+const VIEW_DELETE_USER = 'delete';
+const VIEW_EDIT_USER = 'update';*/
+
+
+
+?>
