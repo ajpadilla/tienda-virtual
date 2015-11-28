d35e9864e7d30e9af915f8aba9831ff6fd39cd28
ajpadilla <ajpadilla88@gmail.com>
2015-11-28 16:15:33 -0430

    Agregando template principal del proyecto

---
+++ site_media/html/index_template.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="">
+	<title>Tienda Virtual</title>
+	<link rel="shortcut icon" size="64x64" href="/site_media/img/shopping-basket-icon.png" />
+	<link rel="stylesheet" type="text/css" href="/site_media/css/styles.css">
+</head>
+<body>
+	<div id="contenedor-encabezado">
+		<div id="contenedor-navegador">
+			<div class="contenedor">
+				<div class="grilla_buscador push_buscador">
+					<input type="text" name="busqueda" id="busqueda" placeholder="¿Buscas algo...?">
+				</div>
+				<div class="grilla_selector push_selector">
+					<select id="categorias">
+						<option>Todas las categorias</option>
+					</select>
+				</div>
+				<div class="grilla_buscar push_buscar"> 
+					<button class="boton-buscar">Buscar</button>
+				</div>
+				<div class="grilla_busqueda_avanzada push_busqueda_avanzada">
+					<a href="#">Búsqueda avanzada</a>
+				</div>
+				<div class="grilla_articulos push_articulos">
+					<a href="#" class="icon-cart"></a>
+					<a href="#" class="icon-checkmark"></a>
+				</div>
+				<div class="grilla_accion push_accion">
+					<button class="boton-buscar">Sing up</button>
+					<button class="boton-buscar">Sing in</button>
+				</div>
+			</div>
+		</div>
+		<div id="contenedor-menu">
+			
+		</div>
+		<a id="logo" href="/site_media/html/index_template.html">Logo</a>
+	</div>
+	<?php echo "string"; ?>
+</body>
+</html>
