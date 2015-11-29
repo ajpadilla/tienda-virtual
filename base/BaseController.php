7546fc09e0b981dd2ba4490b8276a05db432b3cc
ajpadilla <ajpadilla88@gmail.com>
2015-11-28 21:52:51 -0430

    Modificando metodo handler para verificar ruta accedida del sistema

--- base/BaseController.php
+++ base/BaseController.php
@@ -1,9 +1,25 @@
-<?php 
+<?php
+	require_once('constants.php'); 
 	require_once("view.php");
-	
+
+	// Verifica la vista o acción ejecutada por el usuario
 	function handler()
 	{
-		$event = "";
+		$event = VIEW_INDEX;
+		$uri = $_SERVER['REQUEST_URI'];
+		$petitions = array(MODULE_USER.SET_USER,MODULE_USER.VIEW_SET_USER);
+		foreach ($petitions as $petition) 
+		{
+			$uri_petition = $petition.'/';
+			if( strpos($uri, $uri_petition) == true ) 
+			{
+				$action = explode('/', $uri_petition);
+				$event = $action[1];
+			}
+			/*echo $uri.'<br/>';
+			echo $uri_petition.'<br/>';*/
+		}
+		//echo $event;
 		switch ($event)
 		{
 			case 'value':
