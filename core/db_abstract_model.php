fecb988183488c403baa116e1a5ed8d220c5ef28
ajpadilla <ajpadilla88@gmail.com>
2015-11-28 22:32:09 -0430

    Agregando metodos para ejecutar consultas y cargar datos a la base de datos

--- core/db_abstract_model.php
+++ core/db_abstract_model.php
@@ -13,6 +13,47 @@
 		protected $rows = array();
 		private $connection;
 		public $message;
+
+		# métodos abstractos para ABM de clases que hereden    
+	    abstract protected function get();
+	    abstract protected function set();
+	    abstract protected function edit();
+	    abstract protected function delete();
+
+	    protected function open_connection()
+	    {
+	    	$this->connection = new mysqli(self::$db_host, self::$db_user, self::$db_password, self::$db_name);
+	    }
+
+	    private function close_connection()
+	    {
+	    	$this->connection->close();
+	    }
+
+		protected function execute_single_query()
+		{
+			if ($_POST) 
+			{
+				$this->open_connection();
+				$this->connection->query($this->query);
+				$this->close_connection();
+			}
+			else
+			{
+				$this->message = "Metodo no permitido";
+			}
+		}
+
+		protected function get_results_from_query()
+		{
+			$this->open_connection();
+	        $result = $this->connection->query($this->query);
+	        while ($this->rows[] = $result->fetch_assoc());
+	        $result->close();
+	        $this->close_connection();
+	        array_pop($this->rows);
+		}
+
 	}
 
 	
