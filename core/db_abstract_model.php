15d91fcf58080e8c8523aded6a0d286cfe7e1f7c
ajpadilla <ajpadilla88@gmail.com>
2015-11-28 22:17:04 -0430

    Agregando atributos a la clase DBAbstractModel

--- core/db_abstract_model.php
+++ core/db_abstract_model.php
@@ -1,3 +1,19 @@
 <?php 
 
+	/**
+	* 
+	*/
+	abstract class DBAbstractModel
+	{
+		private static $db_host = 'locahost';
+		private static $db_user = 'root';
+		private static $db_password = '1234';
+		protected static $db_name = 'tienda_virtual';
+		protected $query;
+		protected $rows = array();
+		private $connection;
+		public $message;
+	}
+
+	
 ?>
