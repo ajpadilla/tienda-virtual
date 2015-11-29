4a8ac916b334c68a997379e84111caea397319fa
ajpadilla <ajpadilla88@gmail.com>
2015-11-28 21:59:43 -0430

    Agregando plantilla para agregar usuarios

---
+++ site_media/html/add_new_user.php
@@ -0,0 +1,27 @@
+<form id="add_new_user" action="{SET}" method="POST">
+	<br/>
+	<br/>
+	<br/>
+	<br/>
+	<br/>
+	<br/>
+	<label for="email">Email</label>
+	<input type="text" id="email" name="email"/>
+	<label for="username">Nombre de usuario</label>
+	<input type="text" id="username" name="username"/>
+	<label for="password">Contraseña</label>
+	<input type="password" id="password" name="password"/>
+	<label for="confir_pass">Confirmar contraseña</label>
+	<input type="password" id="confir_pass" name="confir_pass"/>
+	<label for="first_name">Nombre</label>
+	<input type="text" id="first_name" name="first_name"/>
+	<label for="last_name">Apellido</label>
+	<input type="text" id="last_name" name="last_name"/>
+	<label for="date_of_birth">Fecha de nacimiento</label>
+	<input type="text" id="date_of_birth" name="date_of_birth"/>
+	<label for="phone">Telefono</label>
+	<input type="text" id="phone" name="phone"/>
+	<label for="address">Direcció</label>
+	<input type="text" id="address" name="address"/>
+	<input type="submit" value="Agregar">
+</form>
