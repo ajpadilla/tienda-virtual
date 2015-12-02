73ce559a7d1f4893d051feb64ca6aef11b4ee24b
ajpadilla <ajpadilla88@gmail.com>
2015-12-02 17:12:46 -0430

    Agregando constante SERVER_ROOT a rutas para controladores, modelos y vistas

--- config.php
+++ config.php
@@ -14,7 +14,7 @@
 	define("WEBSITE", "http://localhost/");
 	define("DS", "/");
 	define("SERVER_ROOT",$_SERVER["DOCUMENT_ROOT"]);
-	define("CONTROLLERS_PATH",APP_ROOT."controllers".DS);
-	define("MODELS_PATH",APP_ROOT."models".DS);
-	define("VIEWS_PATH",APP_ROOT."views".DS);
+	define("CONTROLLERS_PATH",SERVER_ROOT.APP_ROOT."controllers".DS);
+	define("MODELS_PATH",SERVER_ROOT.APP_ROOT."models".DS);
+	define("VIEWS_PATH",SERVER_ROOT.APP_ROOT."views".DS);
 ?>
