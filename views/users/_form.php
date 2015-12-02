bb2c6c01996ba4e2353fec1df8f8129407739261
ajpadilla <ajpadilla88@gmail.com>
2015-12-02 16:31:16 -0430

    Agregando plantilla .php para el formulario de agregar usuariso

---
+++ views/users/_form.php
@@ -0,0 +1,27 @@
+<br/>
+	<br/>
+	<br/>
+	<br/>
+	<br/>
+	<br/>
+	<br/>
+	
+	<label for="email">Email</label>
+	<input type="text" id="email" name="email">
+	<label for="username">Nombre de usuario</label>
+	<input type="text" id="username" name="username">
+	<label for="password">Contraseña</label>
+	<input type="password" id="password" name="password">
+	<label for="confirm_pass">Confirmar contraseña</label>
+	<input type="password" id="confirm_pass" name="confirm_pass">
+	<label for="name">Nombre </label>
+	<input type="text" id="name" name="name">
+	<label for="last_name">Apellido</label>
+	<input type="text" id="last_name" name="last_name">
+	<label for="date_of_birth">Fecha de nacimiento</label>
+	<input type="text" id="date_of_birth" name="date_of_birth">
+	<label for="phone">telefono</label>
+	<input type="text" id="phone" name="phone">
+	<label for="adress">Dirección</label>
+	<input type="text" id="adress" name="adress">
+	<input type="submit" value="Crear">
