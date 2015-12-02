cac677b4bdb9a09f6f6845971dba7b6ff4c2de5f
ajpadilla <ajpadilla88@gmail.com>
2015-12-02 16:27:00 -0430

    reemplazando datos enviados por peticion POST/GET del sistema

--- index.php
+++ index.php
@@ -4,24 +4,37 @@
 	function routing($routes)
 	{
 		$url = $_SERVER['REQUEST_URI'];
-		$url = str_replace("/tienda-virtual.local/","",$url);
+		//echo $url;
+		//$url = str_replace("/tienda-virtual.local/","",$url);
 		$url = str_replace("?".$_SERVER['QUERY_STRING'], "", $url);
-		//echo $_SERVER['QUERY_STRING'];
-		
 		foreach ($routes as $route) 
 		{
-			if ($num_routes = preg_match($route["url"],$url,$matches) > 0) 
+			if ($num_routes = preg_match($route["url"],$url,$matches) > 0)
 			{
+				//$params = array_merge($matches,$params);
 				break;
 			}
 		}
 
-		/*if ($num_routes == 0) 
+		//print_r($matches)."<br/>";
+
+		if ($num_routes == 0) 
 		{
 			exit("No se ha encontrado la ruta");
-		}*/
+		}
+		if ($route["controller"] != "")
+		{
+			include(CONTROLLERS_PATH.$route["controller"].".php");
+		}
 
-		include("/views/"."layouts".DS."default.php");
+		if(file_exists(VIEWS_PATH."layouts".DS.$route["controller"].".php")) 
+		{
+			include(VIEWS_PATH."layouts".DS.$route["controller"].".php");
+		}
+		else
+		{
+			include(VIEWS_PATH."layouts".DS."default.php");
+		}
 
 	}
 
