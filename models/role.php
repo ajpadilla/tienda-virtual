8f8f6e20ed035a783f55d950773de442bae7e34a
ajpadilla <ajpadilla88@gmail.com>
2015-12-05 18:50:24 -0430

    Agregando modelo para roles de usuario

---
+++ models/role.php
@@ -0,0 +1,58 @@
+<?php  
+	/**
+	* 
+	*/
+	class Role extends DBAbstractModel
+	{
+		public $id;
+		public $name;
+		public $display_name;
+		public $description;
+		function __construct()
+		{
+			$this->id = "";
+			$this->name = "";
+			$this->display_name = "";
+			$this->description = "";
+		}
+
+		function __destruct() 
+	    {
+	        unset($this);
+
+	    }
+
+	    public function get($role_id = "")
+	    {
+	    	if ($user_id != "") 
+	    	{
+	    		$this->query = "SELECT * FROM roles 
+	    		WHERE roles.id = '{$role_id}'";
+	    		$this->get_results_from_query();
+	    	}
+
+	    	if (count($this->rows) == 1 ) 
+			{
+				foreach ($this->rows[0] as $property => $value) 
+				{
+					$this->$property = $value;
+				}
+			}
+	    }
+
+	    public function set($role_data = array())
+	    {
+	    	#code
+	    }
+
+	    public  function edit($role_data = array())
+	    {
+	    	#code
+	    }
+	    public  function delete($role_id = '')
+	    {
+	    	#code
+	    }
+
+	}
+?>
