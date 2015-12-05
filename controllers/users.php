dfc88f0a121a9e4479533811d4573312b9373cbc
ajpadilla <ajpadilla88@gmail.com>
2015-12-05 14:11:17 -0430

    Verificando nombre de usuario y email del usuario para ser agregado

--- controllers/users.php
+++ controllers/users.php
@@ -5,8 +5,20 @@
 	{
 		case 'create':
 			$user = new User();
-			$user->set($params["post"]);
-			echo "Usuario agregado";
+			$invalid_fields = $user->search($params["post"]);
+			$errors = get_errors($user->validations, $params["post"]);
+			if (!$errors && !$invalid_fields["email"] && !$invalid_fields["username"]) 
+			{
+				$user->set($params["post"]);
+				save_msg_success("Se ha agregado correctamente el nuevo usuario");
+				redirect_to("users/new");
+			}
+			else
+			{
+				$post = $params["post"];
+				$route["view"] = "new";
+				save_msg_warnings("Por favor, corrige los siguientes campos: <br/><br/>");	
+			}
 			break;
 	}
 ?>
