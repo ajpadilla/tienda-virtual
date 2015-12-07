7849bbf23c98a1ce0b2c44a703cdcf39be094607
ajpadilla <ajpadilla88@gmail.com>
2015-12-07 17:20:31 -0430

    Verificando errores en cada campo del formulario

--- views/users/_form.php
+++ views/users/_form.php
@@ -1,22 +1,27 @@
 <br/>
+	
 	<br/>
 	<br/>
 	<br/>
 	<br/>
 	<br/>
 	<br/>
+	<?php if (isset($errors)) 
+		{
+			var_dump($errors);
+		} 
+	?>
 	<label for="roles">Rol</label>
 	<select name="post[roles]" id="roles">
-		<option value="1">Administrador</option>
-		<option value="2">Vendedor</option>
-		<option value="3">Cliente</option>
+		<option value="1">Vendedor</option>
+		<option value="2">Cliente</option>
 	</select>
 	<label for="email">Email</label>
 	<input type="text" id="email" name="post[email]" value="<?php echo isset($post)?$post['email']:""; ?>">
 	<?php  
-		if (isset($errors)) 
+		if (isset($errors_user)) 
 		{
-			echo error_msg($errors["email"],"Dirección de correo no valida");
+			echo error_msg($errors_user["email"],"Dirección de correo no valida");
 		}
 
 		if(isset($invalid_fields)) {
@@ -26,9 +31,9 @@
 	<label for="username">Nombre de usuario</label>
 	<input type="text" id="username" name="post[username]" value="<?php echo isset($post)?$post['username']:""; ?>">
 	<?php  
-		if (isset($errors)) 
+		if (isset($errors_user)) 
 		{
-			echo error_msg($errors["username"],"Nombre de usuario no valido");
+			echo error_msg($errors_user["username"],"Nombre de usuario no valido");
 		}
 		if(isset($invalid_fields)) {
 			echo error_msg($invalid_fields["username"],"El username ya se encuentra registrado");
@@ -37,9 +42,9 @@
 	<label for="password">Contraseña</label>
 	<input type="password" id="password" name="post[password]" value="<?php echo isset($post)?$post['password']:""; ?>">
 	<?php  
-		if (isset($errors)) 
+		if (isset($errors_user)) 
 		{
-			echo error_msg($errors["password"],"Contraseña no valida");
+			echo error_msg($errors_user["password"],"Contraseña no valida");
 		}
 
 		if (isset($invalid_password["confirm_password"])) 
@@ -50,9 +55,9 @@
 	<label for="confirm_password">Confirmar contraseña</label>
 	<input type="password" id="confirm_password" name="post[confirm_password]">
 	<?php  
-		if (isset($errors)) 
+		if (isset($errors_user)) 
 		{
-			echo error_msg($errors["confirm_password"],"Contraseña no valida");
+			echo error_msg($errors_user["confirm_password"],"Contraseña no valida");
 		}
 
 		if (isset($invalid_password["confirm_password"])) 
@@ -62,18 +67,46 @@
 	?>
 
 	<label for="name">Nombre </label>
-	<input type="text" id="name" name="post[name]">
+	<input type="text" id="name" name="post[name]" value="<?php echo isset($post)?$post['name']:""; ?>"/>
+	<?php 
+		if (isset($errors_person)) 
+		{
+			echo error_msg($errors_person["name"],"Nombre invalido");
+		}
+	?>
 
 	<label for="last_name">Apellido</label>
-	<input type="text" id="last_name" name="post[last_name]">
-
+	<input type="text" id="last_name" name="post[last_name]" value="<?php echo isset($post)?$post['last_name']:""; ?>">
+	<?php 
+		if (isset($errors_person)) 
+		{
+			echo error_msg($errors_person["last_name"],"Apellido invalido");
+		}
+	?>
+	
 	<label for="date_of_birth">Fecha de nacimiento</label>
-	<input type="text" id="date_of_birth" name="post[date_of_birth]">
-
+	<input type="text" id="date_of_birth" name="post[date_of_birth]" value="<?php echo isset($post)?$post['date_of_birth']:""; ?>">
+	<?php 
+		if (isset($errors_person)) 
+		{
+			echo error_msg($errors_person["date_of_birth"],"Fecha invalido");
+		}
+	?>
 	<label for="phone">telefono</label>
-	<input type="text" id="phone" name="post[phone]">
+	<input type="text" id="phone" name="post[phone]" value="<?php echo isset($post)?$post['phone']:""; ?>">
+	<?php 
+		if (isset($errors_person)) 
+		{
+			echo error_msg($errors_person["phone"],"Telefono invalido");
+		}
+	?>
 
 	<label for="address">Dirección</label>
-	<input type="text" id="address" name="post[address]">
-
+	<input type="text" id="address" name="post[address]" value="<?php echo isset($post)?$post['address']:""; ?>">
+	<?php 
+		if (isset($errors_person)) 
+		{
+			echo error_msg($errors_person["address"],"Dirección invalido");
+		}
+	?>
 	<input type="submit" value="Crear">
