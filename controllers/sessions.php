0e5c5968ebf0d24091b7944098c5efaad114b0eb
ajpadilla <ajpadilla88@gmail.com>
2015-12-08 21:44:38 -0430

    Creando acción create

--- controllers/sessions.php
+++ controllers/sessions.php
@@ -1,10 +1,22 @@
 <?php  
-	require(MODELS_PATH."session.php");
+	require(MODELS_PATH."user.php");
 
 	switch ($route["view"]) 
 	{
 		case 'create':
-
+				$user = new User();
+				//$respuesta = $user->login($params["user"]["username"], $params["user"]["password"]);
+				//var_dump($respuesta)."<br/>";
+				if ($user->login($params["user"]["username"], $params["user"]["password"])) 
+				{
+					save_msg_success("Te encuentras logueado");
+					redirect_to("sessions/index");
+				}
+				else
+				{
+					save_msg_warnings("Nombre de usuario y/o contraseña incorrectos");
+					$route["view"] = "new";
+				}
 			break;
 		
 		default:
